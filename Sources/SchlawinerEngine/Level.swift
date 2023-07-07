d1e3c53d32442b21d3c9e86e9d1c8822bb80e48d
Harald Pehl <harald.pehl@gmail.com>
2023-07-07 16:25:16 +0200

    Adjust visibility

--- Sources/SchlawinerEngine/Level.swift
+++ Sources/SchlawinerEngine/Level.swift
@@ -1,5 +1,5 @@
 // Value is the maximum difference
-enum Level: Int {
+public enum Level: Int {
     case easy = 4
     case medium = 2
     case hard = 0
--- Sources/SchlawinerEngine/Settings.swift
+++ Sources/SchlawinerEngine/Settings.swift
@@ -1,13 +1,13 @@
 public struct Settings {
-    static func defauls() -> Settings {
+    public static func defauls() -> Settings {
         Settings(timeout: 60, penalty: 5, retries: 3, numbers: 8, level: .medium)
     }
     
-    var timeout: Int
-    var penalty: Int
-    var retries: Int
-    var numbers: Int
-    var level: Level
+    public var timeout: Int
+    public var penalty: Int
+    public var retries: Int
+    public var numbers: Int
+    public var level: Level
     
     init(timeout: Int, penalty: Int, retries: Int, numbers: Int, level: Level) {
         self.timeout = timeout
